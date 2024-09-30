de4b97bbfc4163883a1f73e75a9ceab6a5cfd934
foroughix <85922868+foroughix@users.noreply.github.com>
2024-09-30 08:32:04 +0330

    Update connect.php

--- WebApp/php/database/connect.php
+++ WebApp/php/database/connect.php
@@ -1,11 +1,11 @@
 <?php
-if(!defined('WebApp')){exit();}
-# DB_HOST, DB_USERNAME, DB_PASSWORD, DB_NAME
-$connect = mysqli_connect('localhost', 'root', '', 'webapp');
-if (!$connect)
-{
-	echo 'Error connecting to database!';
-	exit();
-}
-mysqli_set_charset($connect, 'utf8mb4');
+	if(!defined('WebApp')){exit();}
+	# DB_HOST, DB_USERNAME, DB_PASSWORD, DB_NAME
+	$connect = mysqli_connect('localhost', 'root', '', 'webapp');
+	if (!$connect)
+	{
+		echo 'Error connecting to database (php\database\connect.php)!';
+		exit();
+	}
+	mysqli_set_charset($connect, 'utf8mb4');
 ?> 
