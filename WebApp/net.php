0dbf9f8972bd3e148df8f80dcdb2631672460343
foroughix <85922868+foroughix@users.noreply.github.com>
2024-09-30 08:21:45 +0330

    Update net.php

--- WebApp/net.php
+++ WebApp/net.php
@@ -1,21 +1,21 @@
 <?php
-ob_start();
-define('WebApp', true);
-require_once 'php/session/start.php';
-require_once 'php/cache/control.php';
-require_once 'php/database/connect.php';
-require_once 'php/function/functions.php';
-$q = explode('/', get_url());
-unset($q[0]);
-$w = '';
+  ob_start();
+  define('WebApp', true);
+  require_once 'php/session/start.php';
+  require_once 'php/cache/control.php';
+  require_once 'php/database/connect.php';
+  require_once 'php/function/functions.php';
+  $q = explode('/', get_url());
+  unset($q[0]);
+  $w = '';
 
-require_once 'php/request/net/hello.php';
+  require_once 'php/request/net/hello.php';
 
-ob_end_clean();
-$result = str_replace('	', '', $w);
-echo $result;
-ob_start();
-require_once 'php/database/close.php';
-require_once 'php/session/regenerate.php';
-ob_end_clean();
+  ob_end_clean();
+  $result = str_replace('	', '', $w);
+  echo $result;
+  ob_start();
+  require_once 'php/database/close.php';
+  require_once 'php/session/regenerate.php';
+  ob_end_clean();
 ?>
