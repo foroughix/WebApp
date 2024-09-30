8f0400c30365df2141150422e17e4632ed1bdd5b
foroughix <85922868+foroughix@users.noreply.github.com>
2024-09-30 08:38:07 +0330

    Update start.php

--- WebApp/php/session/start.php
+++ WebApp/php/session/start.php
@@ -1,7 +1,7 @@
 <?php
-if(!defined('WebApp')){exit();}
-$session_time = 604800;
-ini_set('session.cookie_lifetime', $session_time);
-ini_set('session.gc_maxlifetime', $session_time);
-session_start();
+	if(!defined('WebApp')){exit();}
+	$session_time = 604800;
+	ini_set('session.cookie_lifetime', $session_time);
+	ini_set('session.gc_maxlifetime', $session_time);
+	session_start();
 ?> 
